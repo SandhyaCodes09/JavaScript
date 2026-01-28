b0b36c0404b83d29ee5d5b0c62ac1e239f6e61b5
Sandhya Verma <108970976+SandhyaCodes09@users.noreply.github.com>
2026-01-29 00:36:51 +0530

    Update javascript_intro.js

--- javascript-notes/javascript_intro/javascript_intro.js
+++ javascript-notes/javascript_intro/javascript_intro.js
@@ -1,1 +1,15 @@
+// Introduction to JavaScript
+// This is a simple JavaScript program
+
+console.log("Hello, JavaScript!");
+
+// JavaScript can perform calculations
+console.log(10 + 5);
+
+// JavaScript can store values in variables
+let name = "JavaScript";
+let year = 2025;
+
+console.log("Language:", name);
+console.log("Year:", year);
 
